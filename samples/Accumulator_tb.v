9221f4f8e9d01b80114ed200fc32a1c6e9582a80
mtvjr <mtv27@txstate.edu>
2018-04-09 13:47:16 -0500

    Updated finish time calculations

--- samples/Accumulator_tb.v
+++ samples/Accumulator_tb.v
@@ -4,13 +4,22 @@ reg clock;
 reg [0:7] din;
 wire [0:7] dout;
 integer seed = 1;
+integer cycles = 1;
+integer tb_length = 1000;
 
 Accumulator a(clock, din, dout);
 
 always #1 clock <= ~clock;
 
 always @(posedge clock)
+begin
   din <= $random(seed);
+  cycles = cycles + 1;
+end
+
+always @(negedge clock)
+  if (cycles > tb_length)
+    $finish;
 
 initial
 begin
@@ -18,7 +27,6 @@ begin
   $dumpfile("output/accu.vcd");
   $dumpvars(-1, a);
   $monitor("din: %b dout: %b", din, dout);
-  #1024 $finish;
 end
 
 endmodule
