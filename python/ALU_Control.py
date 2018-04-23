e74560651696e94e05bd392c0f4ceac233c103f8
erichmcl <37076259+erichmcl@users.noreply.github.com>
2018-04-22 22:34:06 -0500

    Update ALU_Control.py

--- python/ALU_Control.py
+++ python/ALU_Control.py
@@ -1,11 +1,39 @@
 from myhdl import *
 
 @block
-def ALU_Control(clk, op, out): #Clk and OpCode input, int output
+def ALU_Control(ALUop, funct, ALUcontrol): 
 
-    @always(clk.posedge)
-    def execute():
-        #Insert op code interpretation
-        return
+	@always(ALUop, funct)
+	def control():
+		if (ALUop == 0):
+			ALUcontrol = 0
 
-    return execute
+		elif (ALUop == 1):
+			ALUcontrol = 1
+
+		elif (ALUop == 2):
+			ALUcontrol = 0
+
+		elif (ALUop == 3):
+			ALUcontrol = 1
+
+		elif (ALUop == 4):
+			if (funct == 0):
+				ALUcontrol = 0
+
+			elif (funct == 2):
+				ALUcontrol = 1
+
+			elif (funct == 6):
+				ALUcontrol = 2
+
+return ALU_Control
+
+def JR_Control(alu_op, funct, JRControl):
+
+    if alu_op == 0 and funct == 0x8:
+       JRControl = 1
+   	else
+       JRControl = 0
+
+return JR_Control
